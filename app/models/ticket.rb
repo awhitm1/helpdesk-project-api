8ffa4f60c11041360b0313fa2b81c3052f569733
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-02-18 00:41:53 -0600

    not validating that assigned_tech is true

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,15 @@ class Ticket < ApplicationRecord
   belongs_to :location
   belongs_to :group
   belongs_to :status
+
+  validates :title :description :is_open :user_id :category_id :group_id :location_id :status_id, presence: true
+  validates :assigned_tech_must_be_tech
+
+  private
+
+  def assigned_tech_must_be_tech
+    if assigned_tech_id.present? && !User.find(assigned_tech_id).is_tech
+      errors.add(:assigned_tech_id, 'must be a technician')
+    end
+  end
 end
