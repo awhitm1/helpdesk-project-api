d7479c0ed8f767a881973a89181e27ee6e0a1eb0
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-04 22:04:46 -0600

    had to modify user controller params for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:f_name, :l_name, :email, :password, :password_confirmation)
+    params.permit(:f_name, :l_name, :email, :password, :password_confirmation)
   end
 end
