668503d7bbdb94b99cb6456f8a692dac5e01e1d0
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-17 00:49:34 -0500

    working on getting groups info in user blueprint

--- app/blueprints/user_blueprint.rb
+++ app/blueprints/user_blueprint.rb
@@ -2,7 +2,7 @@
 
 class UserBlueprint < Blueprinter::Base
   identifier :id
-
+  association :groups
   view :normal do
     fields :f_name, :l_name, :email, :is_tech, :is_admin, :active, :groups
   end
