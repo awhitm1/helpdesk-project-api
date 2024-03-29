41ec399380580ff0a1f82910d0a15ea81d2160a3
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-28 19:16:04 -0500

    renamed profile_image_url method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
     render json: UserBlueprint.render(user_current, view: :normal), status: 200
   end
 
-  def cover_image_url
+  def profile_image_url
     rails_blob_url(self.profile_image, only_path: true) if self.profile_image.attached?
   end
 
