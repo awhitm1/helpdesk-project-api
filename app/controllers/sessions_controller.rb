a48e687212894a227200f8540262a98e914ea621
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-04-06 14:05:43 -0500

    change session user blueprint to render_to_hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       token = jwt_encode({user_id: user.id})
-      render json: {token: token, user: UserBlueprint.render(user, view: :normal) }, status: :ok
+      render json: {token: token, user: UserBlueprint.render_to_hash(user, view: :normal) }, status: :ok
     else
       render json: {error: "Session JWT Unauthorized"}, status: :unauthorized
     end
