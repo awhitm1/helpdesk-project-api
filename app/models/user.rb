b240fb0bc7d30ff9d2d83f3115c06ad0580619e3
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-04-08 22:04:37 -0500

    remove validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   attribute :is_admin, :boolean, default: false
   attribute :active, :boolean, default: true
 
-  validates :f_name, :l_name, :email, presence: true, on: :create, on: :update
-  validates :password, :password_confirmation, presence: true, on: :create
-  validates :email, uniqueness: true, if: :do_email_validation?
+  # validates :f_name, :l_name, :email, presence: true, on: :create, on: :update
+  # validates :password, :password_confirmation, presence: true, on: :create
+  # validates :email, uniqueness: true, on: :create, on: :update
 
   has_one_attached :profile_image
 
@@ -19,7 +19,7 @@ class User < ApplicationRecord
     rails_blob_url(self.profile_image, only_path: true) if self.profile_image.attached?
   end
 
-  def do_email_validation?
-    new_record? || email_changed?
-  end
+  # def do_email_validation?
+  #   new_record? || email_changed?
+  # end
 end
