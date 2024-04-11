e9f0249eee62f67412e310f3d43dacfa76db3967
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-04-10 23:12:21 -0500

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_request, except: [:create]
+  before_action :authenticate_request, except: [:create, :default]
   before_action :set_user, only: [:show, :update, :destroy, :upload_image]
 
   def default
